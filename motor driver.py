2143d91217483f3644b157a4052d7c89448df0e3
clement Idemudo <clemcy9@gmail.com>
2022-09-05 16:35:06 +0100

    added motor driver

---
+++ motor driver.py
@@ -0,0 +1,13 @@
+from machine import Pin, ADC, PWM
+from time import sleep
+in1 =Pin(0,Pin.OUT)
+in2 =Pin(1,Pin.OUT)
+
+print(dir(PWM))
+print(dir(ADC))
+
+enA = PWM(Pin(2))
+enA.freq(1000)
+in1.value(0)
+in2.value(1)
+enA.duty_u16(65500)
